0d2357c1d77a61fc6921441db522bcd4a29d530b
alip2216 <alifamunawwar16@gmail.com>
2026-07-14 11:13:47 +0700

    Update API configuration

--- lib/api.config.dart
+++ lib/api.config.dart
@@ -2,4 +2,7 @@ class ApiConfig {
   // Jika Anda pakai Emulator Android, gunakan 10.0.2.2
   // Jika pakai Real Device (HP), gunakan IP Address WiFi laptop Anda
   static const String baseUrl = 'http://192.168.1.81:8000/api';
+
+  // Kunci API Gemini untuk Explore Tab Native
+  static const String geminiApiKey = 'YOUR_API_KEY_HERE';
 }
